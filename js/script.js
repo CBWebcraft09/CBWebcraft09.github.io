18655917a68ef0fcc5b70bab51bcd83d89bb4bbc
Cristian JeanPier Bustamante <cbtryharl0920@gmail.com>
2023-08-07 23:17:35 -0500

    correo

--- js/script.js
+++ js/script.js
@@ -49,6 +49,24 @@ function efectoHabilidades() {
 
 }
 
-
-
 //Funcion de formulario de correo con formspree
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handlesubmit)
+
+async function handlesubmit(event){
+  event.preventDefault()
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers:{
+      'Accept': 'application/json'
+    }
+  })
+  if(response.ok){
+    this.reset()
+    alert('Gracias por contactarme, te escribire pronto')
+  }
+}
