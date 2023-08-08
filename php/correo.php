9ff82169096c3748f0315bcd9fefdfcfae7bdc49
Cristian JeanPier Bustamante <cbtryharl0920@gmail.com>
2023-08-08 00:27:28 -0500

    portafolio

---
+++ php/correo.php
@@ -0,0 +1,18 @@
+<?php
+    if(isset($_POST['enviar'])){
+        if(!empty(($_POST['nombre']) && !empty($_POST['asunto']) && !empty($_POST['mensaje']) && !empty($_POST['email']))){
+            $nombre = $_POST['nombre'];
+            $asunto = $_POST['asunto'];
+            $mensaje = $_POST['mensaje'];
+            $email = $_POST['email'];   
+            $header = "From: norepley@example.com" . "\r\n";
+            $header.= "Reply-To: norepley@example.com" . "\r\n";
+            $header.= "X-Mailer: PHP/". phpversion();
+
+            $mail = @mail($email, $asunto, $mensaje , $header);
+            if($mail){
+                echo "Email enviado existosamente";
+            }
+        }
+    }
+?>
